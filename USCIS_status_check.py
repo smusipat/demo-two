dad305c1fa8896bb57301b67d55942c53776217c
smusipat <143361732+smusipat@users.noreply.github.com>
2024-08-07 14:58:37 -0400

    Update USCIS_status_check.py
    .

--- USCIS_status_check.py
+++ USCIS_status_check.py
@@ -5,17 +5,17 @@ from bs4 import BeautifulSoup
 url = 'https://egov.uscis.gov/'
 search_value = 'EAC123556389'
 
-# Send a POST request with the search value
+# Send a POST request with the search value.
 response = requests.post(url, data={'appReceiptNum': search_value})
 
-# Parse the response HTML
+# Parse the response HTML.
 soup = BeautifulSoup(response.text, 'html.parser')
 
 # Find the status element
 status_element = soup.find('div', class_='current-status-sec')
 
-# Extract the status text
+# Extract the status text.
 status = status_element.text.strip()
 
-# Print the status
+# Print the status.
 print('Status:', status)
